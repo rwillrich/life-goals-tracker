3e628dd5d3c5f7bc8bbe50e3816292a4f92926ad
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2016-02-09 20:44:39 -0200

    Install react-router

--- src/client.jsx
+++ src/client.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
 
 import Counter from './containers/Counter'
 import configureStore from './configureStore'
+
+const routes = (
+  <Route path="/">
+    <IndexRedirect to="dashboard" />
+    <Route path="dashboard" component={Counter} />
+  </Route>
+)
 
 const store = configureStore()
 
 render((
   <Provider store={store}>
-    <Counter />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
 ), document.getElementById('root'))
