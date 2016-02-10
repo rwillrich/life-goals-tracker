368c9162a4dbb3e3a4458f57361ab86551ca45e2
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2016-02-09 22:11:56 -0200

    Refactor configureStore to better communicate intent

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,11 +3,13 @@ import { routerReducer, routerMiddleware } from 'react-router-redux'
 
 import * as reducers from './reducers'
 
-const reducer = combineReducers(Object.assign({}, reducers, {
+const rootReducer = combineReducers(Object.assign({}, reducers, {
   routing: routerReducer
 }))
 
 export default (history, initialState) => {
   const middleware = routerMiddleware(history)
-  return createStore(reducer, initialState, applyMiddleware(middleware))
+  const createStoreWithMiddleware = applyMiddleware(middleware)(createStore)
+
+  return createStoreWithMiddleware(rootReducer, initialState)
 }
