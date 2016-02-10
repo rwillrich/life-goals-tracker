d9b3e9a9e714ff32c7061845453c121201bded65
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2016-02-09 22:00:16 -0200

    Refactor to use class instead of functional components because of HMR

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,18 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 
-const Counter = ({ counter, decrement, increment }) => (
-  <div>
-    <h1><strong>{counter}</strong></h1>
-    <button onClick={decrement}>-</button>
-    <button onClick={increment}>+</button>
-  </div>
-)
+class Counter extends Component {
+  render() {
+    const { counter, decrement, increment } = this.props
+
+    return (
+      <div>
+        <h1><strong>{counter}</strong></h1>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+    )
+  }
+}
 
 Counter.defaultProps = {
   decrement: () => undefined,
