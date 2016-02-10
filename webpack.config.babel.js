372b49fc47184c008700f03d8e7896d8702f91ed
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2016-02-10 17:39:06 -0200

    Fix css files loader

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -16,7 +16,7 @@ export default {
       {
         test: /\.css/,
         loader: 'style!' +
-          'css?modules&localIdentName=[name]__[local]___[hash:base64:5]' +
+          'css?modules&localIdentName=[name]__[local]___[hash:base64:5]!' +
           'postcss'
       }
     ]
